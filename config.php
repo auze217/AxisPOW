e17c5c5c9ebd712f39b3e53daa7c0cdcfd2aad9d
auze217 <auze217@vt.edu>
2018-03-27 18:51:31 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// path constants
+define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
+define('BASE_URL','http://localhost/cs3744/project3/heumanj'); # your base URL should be http://localhost/cs3744/project3/heumanj
+
+
+define('DB_USER', 'root');
+define('DB_HOST', 'localhost');
+define('DB_DATABASE', 'mythologytree');
+define('DB_PASS', '');
